Blackmanx/android_device_realme_sm8250-common

sm8250-common: Run oplusSensor hal as root user
For the hal not to be denied access to oplusreserve partitions

extract-files.sh
@@ -68,6 +68,9 @@ function blob_fixup() {
     disabled
 EOF
             ;;
+        odm/etc/init/vendor.oplus.hardware.oplusSensor@1.0-service.rc)
+            sed -i "/user/ s/system/root/g" "${2}"
+            ;;
         odm/lib/liblvimfs_wrapper.so|odm/lib64/libCOppLceTonemapAPI.so|vendor/lib64/libalsc.so)
             "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
             ;;

Badr98-t/devcie-realme-sm8250

extract-files.sh
@@ -66,6 +66,9 @@ function blob_fixup() {
         odm/etc/dolby/multimedia_dolby_dax_default.xml)
             sed -i "/volume-leveler-enable/ s/true/false/g" "${2}"
             ;;
+        dm/etc/init/vendor.oplus.hardware.oplusSensor@1.0-service.rc)
+            sed -i "/user/ s/system/root/g" "${2}"
+            ;;
         odm/lib/libdlbdsservice_v3_6.so|odm/lib/libstagefright_soft_ddpdec.so|odm/lib/libstagefrightdolby.so|odm/lib64/libdlbdsservice_v3_6.so)
             "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
             ;;